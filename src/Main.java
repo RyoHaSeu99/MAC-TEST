84ad7d7a37356d46a7ff9b9b12f1f294e6396eb0
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-08 00:36:17 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                System.out.println(i + " X " + j + " = " + i * j);
-            }
-            System.out.println("-------------");
+        final int DAYS_IN_WEEK = 7;
 
-        }
+        int DAYS_IN_WEEK = 8;
+
+        System.out.println(DAYS_IN_WEEK);
+
+
         }
 }
