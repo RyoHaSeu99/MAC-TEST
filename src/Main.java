702b1f8a56ace077f7cfefa378973e4b36b1ee4d
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-08 00:36:36 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         final int DAYS_IN_WEEK = 7;
 
-        int DAYS_IN_WEEK = 8;
+
 
         System.out.println(DAYS_IN_WEEK);
 
