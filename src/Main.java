af77e317d5fed0bbfefc958f18fa9fbc8cd9310d
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-07 23:57:43 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
                 System.out.println(i + " X " + j + " = " + i * j);
             }
             System.out.println("-------------");
-        }
+
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= 9; j++) {
+                    System.out.println(i + " X " + j + " = " + i * j);
+                }
+                System.out.println("-------------");
+
+            }
     }
 }
